11004678c28c6df29788b07378d7c7d54cbefdf1
Sasawat Yimleang <122139640+SYimleang@users.noreply.github.com>
2025-05-20 11:36:29 -0400

    Update top-languages-card.js

--- src/cards/top-languages-card.js
+++ src/cards/top-languages-card.js
@@ -279,7 +279,7 @@ const createLanguageTextNode = ({ langs, totalSize, hideProgress }) => {
     );
     return flexLayout({
       items,
-      gap: 25,
+      gap: 10,
       direction: "column",
     }).join("");
   });
@@ -352,7 +352,7 @@ const renderNormalLayout = (langs, width, totalLanguageSize) => {
  * @returns {string} Compact layout card SVG object.
  */
 const renderCompactLayout = (langs, width, totalLanguageSize, hideProgress) => {
-  const paddingRight = 20;
+  const paddingRight = 50;
   const offsetWidth = width - paddingRight;
   // progressOffset holds the previous language's width and used to offset the next language
   // so that we can stack them one after another, like this: [--][----][---]
