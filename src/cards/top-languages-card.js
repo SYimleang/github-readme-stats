7515e00d6e7470d72ec0ec656fc1f739b1e6953f
Sasawat Yimleang <122139640+SYimleang@users.noreply.github.com>
2025-05-20 12:29:17 -0400

    Update top-languages-card.js

--- src/cards/top-languages-card.js
+++ src/cards/top-languages-card.js
@@ -266,7 +266,7 @@ const createCompactLangNode = ({ lang, totalSize, hideProgress, index }) => {
  */
 const createLanguageTextNode = ({ langs, totalSize, hideProgress }) => {
   const longestLang = getLongestLang(langs);
-  const chunked = chunkArray(langs, langs.length / 2);
+  const chunked = chunkArray(langs, Math.ceil(langs.length / 3);
   const layouts = chunked.map((array) => {
     // @ts-ignore
     const items = array.map((lang, index) =>
@@ -285,7 +285,7 @@ const createLanguageTextNode = ({ langs, totalSize, hideProgress }) => {
   });
 
   const percent = ((longestLang.size / totalSize) * 100).toFixed(2);
-  const minGap = 150;
+  const minGap = 100;
   const maxGap = 20 + measureText(`${longestLang.name} ${percent}%`, 11);
   return flexLayout({
     items: layouts,
